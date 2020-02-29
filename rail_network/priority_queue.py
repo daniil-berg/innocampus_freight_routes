f278f2ffb33acc46fd95478677eed5188c1584e6
df <df@fajnberg.de>
2020-02-29 11:52:20 +0100

    some slight improvements, private attr __last_node

--- rail_network/priority_queue.py
+++ rail_network/priority_queue.py
@@ -11,6 +11,7 @@ class LinkedList(MutableSequence):
     def __init__(self):
         self.__start = None
         self.__length = 0
+        self.__last_node = None
 
     def __repr__(self) -> str:
         s = '['
@@ -36,14 +37,13 @@ class LinkedList(MutableSequence):
         return next_node.data
 
     def __setitem__(self, idx: int, data: Any) -> None:
-        self._validate_idx(idx, append_flag=True)
-        current_node = self.__start
-        for _ in range(idx):
-            if current_node.next is not None:
-                current_node = current_node.next
-        if idx == self.__length:
-            current_node.next = self.Node(data=data)
+        if idx == self.__length - 1:
+            self.__last_node.data = data
         else:
+            self._validate_idx(idx)
+            current_node = self.__start
+            for _ in range(idx):
+                current_node = current_node.next
             current_node.data = data
 
     def __delitem__(self, idx: int) -> None:
@@ -73,11 +73,31 @@ class LinkedList(MutableSequence):
         else:
             previous_node.next = new_node
         self.__length += 1
+        if idx == self.__length:
+            self.__last_node = new_node
+
+    def append(self, data: Any) -> None:
+        new_node = self.Node(data=data)
+        if self.__length == 0:
+            self.__start = new_node
+        else:
+            self.__last_node.next = new_node
+        self.__length += 1
+        self.__last_node = new_node
 
     def extend(self, iterable: Iterable[Any]) -> None:
         if isinstance(iterable, LinkedList):
             iterable = tuple(iterable)
         super().extend(iterable)
+
+
+class PriorityQueue(LinkedList):
+    def __init__(self, cmp_attr: str = None):
+        super().__init__()
+        self.__cmp_attr = cmp_attr
+
+    def insert(self, idx: int, data: Any) -> None:
+        pass
 
 
 if __name__ == '__main__':
@@ -87,6 +107,7 @@ if __name__ == '__main__':
     mylist.insert(1, 'B')
     print(str(mylist))
     print(repr(mylist))
+    mylist[2] = 'test'
     otherlist = LinkedList()
     otherlist.append('c')
     otherlist.append('d')
@@ -95,4 +116,6 @@ if __name__ == '__main__':
     mylist.extend(['e', 'f'])
     print(mylist)
     mylist.extend(mylist)
+    print(mylist)
+    mylist[5] = 18
     print(mylist)
