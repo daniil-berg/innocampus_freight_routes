152ce9dc5733b9b1e7acbbb446b68fc8614205af
daniil <mail@daniil.fajnberg.de>
2020-03-03 16:52:26 +0100

    response specificity

--- rail_network/views_api.py
+++ rail_network/views_api.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import Map, Node, Link
@@ -14,21 +14,25 @@ def shortest_path(request: Request) -> Response:
     start_id = request.query_params.get('start')
     end_id = request.query_params.get('end')
     if start_id is None or end_id is None:
-        return Response(status=HTTP_400_BAD_REQUEST)
-    try:
-        start_id = int(start_id)
-        end_id = int(end_id)
-    except ValueError:
-        return Response(status=HTTP_400_BAD_REQUEST)
+        return Response("Both 'start' and 'end' are required", status=HTTP_400_BAD_REQUEST)
+    start_id = int(start_id)
+    end_id = int(end_id)
+
     # TODO: map_id currently defaults to last Map if none was passed!
     map_id = request.query_params.get('map', Map.objects.last().pk)
     map_obj = get_object_or_404(Map, id=map_id)
+
+    if not Node.objects.filter(id=start_id, map=map_obj).exists():
+        return Response("Node ID {} on Map ID {} does not exist".format(start_id, map_id), status=HTTP_404_NOT_FOUND)
+    if not Node.objects.filter(id=end_id, map=map_obj).exists():
+        return Response("Node ID {} on Map ID {} does not exist".format(end_id, map_id), status=HTTP_404_NOT_FOUND)
+
     algorithm = request.query_params.get('algorithm', 'dijkstra')
     if algorithm == 'dijkstra':
         dist, path = map_obj.dijkstra_from_to(start=start_id, end=end_id)
         return Response(data={'dist': dist, 'path': path})
     else:
-        return Response(status=HTTP_400_BAD_REQUEST)
+        return Response("Algorithm '{}' not valid or not implemented".format(algorithm), status=HTTP_400_BAD_REQUEST)
 
 
 class NodeListCreateAPIView(ListCreateAPIView):
