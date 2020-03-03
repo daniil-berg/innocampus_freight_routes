a1691d4321bfd72283a622366669dd08f83fec34
daniil <mail@daniil.fajnberg.de>
2020-03-03 13:00:38 +0100

    view last() Map, doc note; pipfile now versioned

--- rail_network/views.py
+++ rail_network/views.py
@@ -4,9 +4,12 @@ from .models import Map
 
 
 def new_map(request):
+    """
+    Currently, this just loads the Map (i.e. Graph) last saved in the database into the context in Cytoscape.js format
+    """
     init_elements = []
     if Map.objects.all().count() != 0:
-        map_obj = Map.objects.first()
+        map_obj = Map.objects.last()
         init_elements = map_obj.cytoscape_elements_list
     context = {
         'init_elements': init_elements,
