c3b31203bafb0693abc6e85e1b47626b1c0be36e
daniil <mail@daniil.fajnberg.de>
2020-03-03 15:55:26 +0100

    pass map id

--- rail_network/views.py
+++ rail_network/views.py
@@ -7,11 +7,14 @@ def graph_map(request):
     """
     TODO: Currently, this just loads the Map (i.e. Graph) last saved in the database into the context in Cytoscape.js format
     """
-    init_elements = []
     if Map.objects.all().count() != 0:
         map_obj = Map.objects.last()
         init_elements = map_obj.cytoscape_elements_list
+    else:
+        map_obj = Map.objects.create()
+        init_elements = []
     context = {
         'init_elements': init_elements,
+        'map_id': map_obj.pk,
     }
     return render(request, 'rail_network/map.html', context=context)
