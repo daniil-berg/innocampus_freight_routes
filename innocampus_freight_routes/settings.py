589aa167873e7fb62167d10ad54aebe070a52cf2
daniil <mail@daniil.fajnberg.de>
2020-03-03 13:53:06 +0100

    REST framework

--- innocampus_freight_routes/settings.py
+++ innocampus_freight_routes/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rail_network',
     'users',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
