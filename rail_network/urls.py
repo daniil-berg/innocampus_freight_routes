953e9a849c839cc1bd4d822fa6b0d9ff66333496
daniil <mail@daniil.fajnberg.de>
2020-03-03 14:43:32 +0100

    working cities retrieve api

--- rail_network/urls.py
+++ rail_network/urls.py
@@ -3,5 +3,6 @@ from . import views, views_api
 
 urlpatterns = [
     path('', views.graph_map, name='graph_map'),
+    path('api/cities/<int:pk>/', views_api.CityRetrieveUpdateDestroyAPIView.as_view()),
     path('api/cities/', views_api.CityListCreateAPIView.as_view()),
 ]
--- rail_network/views_api.py
+++ rail_network/views_api.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import Map, City
 from .serializers import CitySerializer
@@ -7,5 +7,10 @@ from .serializers import CitySerializer
 # TODO: Here, as with the regular views, everything returned is always related to the last Map object
 
 class CityListCreateAPIView(ListCreateAPIView):
+    queryset = City.objects.filter(node__map=Map.objects.last())
+    serializer_class = CitySerializer
+
+
+class CityRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = City.objects.filter(node__map=Map.objects.last())
     serializer_class = CitySerializer
