26cb4dad41b425d864551ac70c03d415ea7856a1
daniil <mail@daniil.fajnberg.de>
2020-03-03 15:31:58 +0100

    node serializer update seems to be working - PUT and PATCH seem to work

--- rail_network/serializers.py
+++ rail_network/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from .models import Node, Link, City
@@ -8,12 +9,6 @@ class CitySerializer(serializers.ModelSerializer):
         model = City
         fields = ('id', 'name', 'date_created', 'date_updated')
 
-    def create(self, validated_data):
-        city_data = validated_data.pop('city')
-        City.objects.create_city_node(**validated_data)
-
-    # def update(self, instance, validated_data):
-
 
 class NodeSerializer(serializers.ModelSerializer):
     city = CitySerializer()
@@ -22,3 +17,21 @@ class NodeSerializer(serializers.ModelSerializer):
         model = Node
         fields = ('id', 'map', 'pos_h', 'pos_v', 'city', 'direct_successors', 'date_created', 'date_updated')
 
+    def create(self, validated_data):
+        city_data = validated_data.pop('city')
+        with transaction.atomic():
+            new_node = Node.objects.create(**validated_data)
+            City.objects.create(node=new_node, **city_data)
+        return new_node
+
+    def update(self, instance, validated_data):
+        instance.pos_h = validated_data.get('pos_h', instance.pos_h)
+        instance.pos_v = validated_data.get('pos_v', instance.pos_v)
+        instance.save()
+        if 'city' in validated_data.keys():
+            if instance.city is not None:
+                instance.city.name = validated_data['city'].get('name', instance.city.name)
+                instance.city.save()
+            else:
+                City.objects.create(node=instance, **validated_data['city'])
+        return instance
